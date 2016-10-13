307ff9bf03cd588f29ee47dcfd5ea0baa2335316
ob00042 <ob00042@gmail.com>
2016-10-13 20:10:09 +0300

    Add select flight

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,35 +1,12 @@
 class FlightsController < ApplicationController
 
 	def index
-		@flight = Flight.new
-		@flights = Flight.all
-		@from_airports = from_airports(@flights)
-		@to_airports = to_airports(@flights)
-		@dates = dates(@flights)
-		@airports = Airport.all
+		@results = []
+		@airport_codes = Airport.all.collect{ |a| [a.code, a.id] }
+		@dates = Flight.flight_dates
+		@results << Flight.where(from_airport_id: params[:from].to_i, to_airport_id: params[:to].to_i)
 	end
 
-	def to_airports(flights)
-		to_airports = []
-		flights.each do |flight|
-			to_airports << flight.to_airport.code
-		end
-		to_airports
-	end
-
-	def from_airports(flights)
-		from_airports = []
-		flights.each do |flight|
-			from_airports << flight.from_airport.code
-		end
-		from_airports
-	end
-
-	def dates(flights)
-		dates = []
-		flights.each do |flight|
-			dates << flight.date
-		end
-		dates
-	end
+	
+	
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,4 +2,11 @@ class Flight < ApplicationRecord
 	belongs_to :from_airport, class_name: "Airport"
 
 	belongs_to :to_airport, class_name: "Airport"
+
+	def self.flight_dates
+		all.collect{ |f| f.date.strftime("%m/%d/%Y at %H:%M") }
+	end
+
+	
+
 end
